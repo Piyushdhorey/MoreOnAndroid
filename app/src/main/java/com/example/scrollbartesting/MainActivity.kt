7540042512244b5da10aaeb6d105c03aa9a6ee4f
P-CXR <pdhorey@cxr.agency>
2023-09-10 12:55:33 +0530

    Scroll Bar layout design with Scroll view

---
+++ app/src/main/java/com/example/scrollbartesting/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.scrollbartesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ScrollView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val scrollView = findViewById<ScrollView>(R.id.scrollView)
+        scrollView.isScrollbarFadingEnabled = false // Disable scrollbar fading
+        scrollView.scrollBarFadeDuration = 2000
+    }
+}
